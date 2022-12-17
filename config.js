48667b34b46291672564d5c4c69ae6519cb9636b
= <40185507+Arkezz@users.noreply.github.com>
2022-12-17 08:08:58 +0200

    commit

--- config.js
+++
@@ -1,6 +0,0 @@
-const path = require("path");
-
-export const dataDirectory = (type) =>
-  path.join(__dirname, "../assets/data", type);
-export const imagesDirectory = (type) =>
-  path.join(__dirname, "../assets/images", type);
