e34af09a9e982bcd8d5af6af32c9117b88e31bc2
= <40185507+Arkezz@users.noreply.github.com>
2023-04-14 21:04:56 +0200

    ignore for now

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -31,5 +31,64 @@ describe("GameController routes", () => {
     });
   });
 
+  describe("GET /:type", () => {
+    it("should return all entities of a type", (done) => {
+      // Mock the GameController.getEntities() function
+      const entities = [
+        "blade-warden",
+        "foe-breaker",
+        "keen-strider",
+        "spell-weaver",
+        "twin-striker",
+      ];
+      const getEntitiesStub = sinon
+        .stub(GameController, "getEntities")
+        .resolves(entities);
+
+      supertest(app.callback())
+        .get("/classes")
+        .expect(200)
+        .end((err, res) => {
+          if (err) return done(err);
+
+          // Check if the response body matches the expected result
+          expect(res.body).to.deep.equal(entities);
+
+          // Restore the GameController.getEntities() function
+          getEntitiesStub.restore();
+          done();
+        });
+    });
+  });
+
+  describe("GET /:type/:id", () => {
+    it("should return an entity of a type", (done) => {
+      // Mock the GameController.getEntity() function
+      const entity = {
+        id: "blade-warden",
+        name: "Blade Warden",
+        description: "A master of the blade.",
+        image: "blade-warden.png",
+      };
+      const getEntityStub = sinon
+        .stub(GameController, "getEntity")
+        .resolves(entity);
+
+      supertest(app.callback())
+        .get("/classes/blade-warden")
+        .expect(200)
+        .end((err, res) => {
+          if (err) return done(err);
+
+          // Check if the response body matches the expected result
+          expect(res.body).to.deep.equal(entity);
+
+          // Restore the GameController.getEntity() function
+          getEntityStub.restore();
+          done();
+        });
+    });
+  });
+
   // Add more tests for other routes here
 });
