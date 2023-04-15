325c2da6705023087432fea582aba7d51971776a
= <40185507+Arkezz@users.noreply.github.com>
2023-04-16 01:06:02 +0200

    Maybe getting somewhere?

--- src/tests/routes.spec.ts
+++ src/tests/routes.spec.ts
@@ -8,3 +8,15 @@ test("GET / returns types from GameController.getTypes", async (t) => {
   t.is(response.status, 200);
   t.deepEqual(response.body, ["classes", "enemies", "quests", "weapons"]);
 });
+
+test("GET /:type returns entities from GameController.getEntities", async (t) => {
+  const response = await request(app.callback()).get("/classes");
+  t.is(response.status, 200);
+  t.true(Array.isArray(response.body));
+});
+
+test("GET /:type/all returns all entities from GameController.getAllEntities", async (t) => {
+  const response = await request(app.callback()).get("/classes/all");
+  t.is(response.status, 200);
+  t.true(Array.isArray(response.body));
+});
